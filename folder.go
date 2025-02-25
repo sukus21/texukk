5ff7490997024a3c78b3b1b53996ff5490c135af
sukus <sukus21@gmail.com>
2025-02-25 22:55:25 +0100

    use fs.WalkDir for iterating

--- folder.go
+++ folder.go
@@ -3,50 +3,47 @@ package texukk
 import (
 	"errors"
 	"image"
+	"io/fs"
 	"os"
-	"path"
 )
 
 func NewAtlasFromFolder(srcPath string, width int, height int, recursive bool) (*Atlas, error) {
-	folders := []string{srcPath}
+	fsys := os.DirFS(srcPath)
 	atlas := NewAtlas(width, height)
 	errs := []error{}
 
-	for i := range folders {
-		folderName := folders[i]
-		folder, err := os.Open(folderName)
+	fs.WalkDir(fsys, ".", func(fname string, d fs.DirEntry, err error) error {
 		if err != nil {
 			errs = append(errs, err)
-			continue
+			return nil
 		}
-		content, err := folder.ReadDir(0)
-		if err != nil {
-			errs = append(errs, err)
-			continue
+
+		// This is probably an image
+		if !d.IsDir() {
+			file, err := fsys.Open(fname)
+			if err != nil {
+				errs = append(errs, err)
+				return nil
+			}
+			img, _, err := image.Decode(file)
+			file.Close()
+			if err != nil {
+				errs = append(errs, err)
+				return nil
+			}
+			atlas.AddNamed(img, fname)
 		}
-		for _, v := range content {
-			srcPath := path.Join(folderName, v.Name())
-			if v.IsDir() {
-				if recursive {
-					folders = append(folders, srcPath)
-				}
-			} else {
-				file, err := os.Open(srcPath)
-				if err != nil {
-					errs = append(errs, err)
-					continue
-				}
-				img, _, err := image.Decode(file)
-				file.Close()
-				if err != nil {
-					errs = append(errs, err)
-					continue
-				}
-				atlas.AddNamed(img, srcPath)
+
+		// Directory, what do?
+		if d.IsDir() {
+			if !recursive && fname != "." {
+				return fs.SkipDir
 			}
 		}
-		folder.Close()
-	}
+
+		// All good
+		return nil
+	})
 
 	return atlas, errors.Join(errs...)
 }
